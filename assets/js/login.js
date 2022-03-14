b9c613975711b6517eefaaa484cff2b8983939ec
小陆 <2574360820@qq.com>
2022-03-14 21:32:23 +0800

    完成了登录和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,13 @@
-$(function() {
+$(function () {
+
   // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
+  $('#link_reg').on('click', function () {
     $('.login-box').hide()
     $('.reg-box').show()
   })
 
   // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
+  $('#link_login').on('click', function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
@@ -14,16 +15,18 @@ $(function() {
   // 从 layui 中获取 form 对象
   var form = layui.form
   var layer = layui.layer
+
   // 通过 form.verify() 函数自定义校验规则
   form.verify({
     // 自定义了一个叫做 pwd 校验规则
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
     // 校验两次密码是否一致的规则
-    repwd: function(value) {
+    repwd: function (value) {
       // 通过形参拿到的是确认密码框中的内容
       // 还需要拿到密码框中的内容
       // 然后进行一次等于的判断
       // 如果判断失败,则return一个提示消息即可
+      //$('.reg-box [name=password]') 意思是：在reg-box里面 name=password 的元素
       var pwd = $('.reg-box [name=password]').val()
       if (pwd !== value) {
         return '两次密码不一致！'
@@ -32,7 +35,7 @@ $(function() {
   })
 
   // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
+  $('#form_reg').on('submit', function (e) {
     // 1. 阻止默认的提交行为
     e.preventDefault()
     // 2. 发起Ajax的POST请求
@@ -40,8 +43,9 @@ $(function() {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val()
     }
-    $.post('/api/reguser', data, function(res) {
+    $.post('/api/reguser', data, function (res) {
       if (res.status !== 0) {
+        //layer 在上面有注册
         return layer.msg(res.message)
       }
       layer.msg('注册成功，请登录！')
@@ -51,15 +55,16 @@ $(function() {
   })
 
   // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
+  $('#form_login').submit(function (e) {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
+      //http://ajax.frontend.itheima.net
       url: '/api/login',
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
